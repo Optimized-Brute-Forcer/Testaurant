f9390d8fcbf1dd6a2edb60fa3afbd9f422bf6716
Ashish Singh <ashish.singh@lendingkart.com>
2025-12-30 15:53:08 +0530

    redirects

--- testaurant_ui/vite.config.ts
+++ testaurant_ui/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://testaurant-be.onrender.com',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/testaurant/v1'),
       },
